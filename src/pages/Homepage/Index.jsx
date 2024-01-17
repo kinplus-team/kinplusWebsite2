fa85da6ecdca0a7498590c44309405294e0b6674
Nwosa <obere4u@gmail.com>
2024-01-17 06:24:47 +0100

    delete duplicate folder

--- src/pages/Homepage/Index.jsx
+++ src/pages/Homepage/Index.jsx
@@ -2,10 +2,10 @@
 import HeroPage from "../../components/HeroPage/Index";
 import PartnersAndSponsors from "../../components/PartnersAndSponsors/Index";
 import WhyChooseUs from "../../components/WhyChooseUs/Index";
-import WhatWeDo from "../../components/whatWeDoComponent/WhatWeDo/Index";
 import TrainingComponent from "../../components/trainingPrograms/TrainingComponent/Index";
 import TrainingTestimonial from "../Training/TrainingTestimonial/Index";
 import FAQ from "../Training/FAQ/Index";
+import WhatWeDoCard from "../../components/whatWeDoComponent/WhatWeDoCard/Index";
 
 export default function Homepage() {
   return (
@@ -20,7 +20,7 @@ export default function Homepage() {
       <WhyChooseUs />
 
       {/*whatWeDo Component */}
-      <WhatWeDo />
+      <WhatWeDoCard />
 
       {/*Training Component */}
       <TrainingComponent />
