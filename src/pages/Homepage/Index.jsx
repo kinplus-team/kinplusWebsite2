31ed973803803ccc215243cebca573d1db3dacf6
Aghanokpe <atelohor@gmail.com>
2024-03-26 13:39:58 +0100

    Update code

--- src/pages/Homepage/Index.jsx
+++ src/pages/Homepage/Index.jsx
@@ -1,6 +1,5 @@
 import HeroPage from "../../components/HeroPage/Index";
 import PartnersAndSponsors from "../../components/PartnersAndSponsors/Index";
-import WhyChooseUs from "../../components/WhyChooseUs/Index";
 import TrainingComponent from "../../components/trainingPrograms/TrainingComponent/Index";
 import TrainingTestimonial from "../Training/TrainingTestimonial/Index";
 import FAQ from "../Training/FAQ/Index";
@@ -18,7 +17,7 @@ export default function Homepage() {
       <PartnersAndSponsors />
 
       {/*WhyChooseUs Component */}
-      <WhyChooseUs />
+      
 
       {/*whatWeDo Component */}
       <WhatWeDo />
