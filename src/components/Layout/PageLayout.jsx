89b1a5c962310568b8794d02d9fa4ebd7d524d6f
Stephen <nwabuezesc@gmail.com>
2024-05-01 20:51:19 +0100

    add conditions to switch section background color

--- src/components/Layout/PageLayout.jsx
+++ src/components/Layout/PageLayout.jsx
@@ -1,6 +1,7 @@
-export default function PageLayout({ children, className, bgColor }) {
+export default function PageLayout({ children, className, isBgColorWhite }) {
+  console.log(isBgColorWhite);
   return (
-    <div className={`${bgColor}`}>
+    <div className={!isBgColorWhite && "bg-[#f1f3f9]"}>
       <div className={` lg:px-[30px] px-4 max-w-[93rem] mx-auto ${className} `}>
         {children}
       </div>
