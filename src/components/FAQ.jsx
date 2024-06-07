1e78f56e4a0a7fa675400928aa5280c7034354a1
Stephen <nwabuezesc@gmail.com>
2024-06-07 14:20:56 +0100

    commit

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -29,37 +29,30 @@ export default function FAQComponent({ FAQs }) {
           product development and training opportunities here.
         </Text>
       </div>
-      <div className="grid gap-5 pt-[30px]">
+      <div className="grid lg:grid-cols-3 gap-14 pt-[30px]">
         {FAQs.map((FAQ, i) => (
           <div
             key={i}
             onClick={() => toggleFAQ(i)}
-            className="w-full mx-auto cursor-pointer"
+            className="grid  max-w-md grid-rows-[auto_auto_1fr] gap-3 w-full mx-auto cursor-pointer"
           >
-            <div className="rounded-[10px] grid py-[14px] grid-cols-[1fr_auto]">
-              <Text type="paragraph" className="text-[#556987]">
-                {FAQ.question}
-              </Text>
+            <p className="text-[#2A3342] text-[20px] font-medium leading-[30px]">
+              {FAQ.question}
+            </p>
 
-              {showAns && index === i ? (
-                <FaMinus className="w-6 h-6 lg:place-self-end place-self-center cursor-pointer" />
-              ) : (
-                <FaPlus className="w-6 h-6 lg:place-self-end place-self-center cursor-pointer" />
-              )}
-            </div>
+            <Text type="subparagraph" className="text-[#556987]">
+              {FAQ.ans.summary}
+            </Text>
 
-            {showAns && index === i && (
-              <div>
-                <Text type="subparagraph" className="px-5 text-[#556987]">
-                  {FAQ.ans.summary}
-
-                  {FAQ.ans.options &&
-                    FAQ.ans.options.map((option, i) => (
-                      <Text type="subparagraph">{option}</Text>
-                    ))}
-                </Text>
-              </div>
-            )}
+            <ul>
+              {FAQ.ans.options &&
+                FAQ?.ans?.options.map((option, i) => (
+                  <li className="text-[#556987] my-1 list-disc ml-5">
+                    {" "}
+                    {option}
+                  </li>
+                ))}
+            </ul>
           </div>
         ))}
       </div>
