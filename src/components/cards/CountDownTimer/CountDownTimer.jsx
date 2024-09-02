5211c21f39b792d739a85dcf3d59797215d10aac
Stephen <nwabuezesc@gmail.com>
2024-09-02 11:40:44 +0100

    commit

--- src/components/cards/CountDownTimer/CountDownTimer.jsx
+++ src/components/cards/CountDownTimer/CountDownTimer.jsx
@@ -11,20 +11,18 @@ export default function CountDownTimer({ image }) {
       <div className="lg:block hidden">
         <DesktopView
           image={image.desktopImg}
-          time={`00:00:00:00`}
+          time={`${formatNumber(days)}:${formatNumber(hours)}:${formatNumber(
+            minutes
+          )}:${formatNumber(seconds)}`}
         />
-          {/* time={`${formatNumber(days)}:${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`}
-         /> */}
       </div>
 
       <div className="lg:hidden block">
         <MobileView
           image={image.mobileImg}
-          time={`00:00:00
-          :00`}
-          // time={`${formatNumber(days)}:${formatNumber(hours)}:${formatNumber(
-          //   minutes
-          // )}:${formatNumber(seconds)}`}
+          time={`${formatNumber(days)}:${formatNumber(hours)}:${formatNumber(
+            minutes
+          )}:${formatNumber(seconds)}`}
         />
       </div>
     </div>
