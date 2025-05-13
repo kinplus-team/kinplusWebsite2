7755acf2ce8559e62bd5c9daa70ae01f8ee3dfdc
Nwosa <obere4u@gmail.com>
2025-05-13 15:41:14 +0100

    feat: Update Terms and Conditions page with additional guidelines and change list format

--- src/pages/terms-and-conditions/TermsAndCondition.jsx
+++ src/pages/terms-and-conditions/TermsAndCondition.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function TermsAndConditions() {
   return (
-    <main className="bg-black bg-opacity-90 text-white min-h-screen flex flex-col items-center px-4 py-10">
+    <main className="bg-black bg-opacity-90 text-white min-h-screen flex flex-col items-center px-4 py-10 lg:py-20">
       <h2 className="text-2xl md:text-3xl font-semibold underline mb-8 text-center">
         Premium Training Terms and Conditions
       </h2>
-      <ol className="list-decimal space-y-4 max-w-3xl text-lg pl-8">
+      <ul className="list-decimal space-y-4 max-w-3xl text-lg pl-8">
         <li>
           Payment in installments requires that you pay up within the first two
           months of the training (for the 3-month package) or the first three
@@ -23,8 +23,8 @@ export default function TermsAndConditions() {
         </li>
         <li>
           If you're absent from your training program for a period of up to 20
-          working days at a stretch without the consent of the organization,
-          your training program will be considered 'Abandoned'.
+          working days at a stretch without the clear awareness of the
+          organization, your training program will be considered 'Abandoned'.
         </li>
         <li>
           To reactivate an 'Abandoned' training program, you will be required to
@@ -38,8 +38,8 @@ export default function TermsAndConditions() {
         </li>
         <li>
           In addition to any special requirements by each training track,
-          certification requires complete payment for the training and the
-          certificate fee.
+          training certification requires complete payment for the training and
+          the certificate fee.
         </li>
         <li>You will be required to pay a sum of #5,000 for certification.</li>
         <li>
@@ -47,7 +47,27 @@ export default function TermsAndConditions() {
           for a returning trainee. Otherwise, the regular payment fees apply to
           all applicants.
         </li>
-      </ol>
+        <li>
+          NO use of personal electrical extension boxes, unless properly
+          assessed by the hardware office. Submit any such material to the
+          hardware office for proper assessment.
+        </li>
+        <li>Avoid the use of naked wires with any gadget.</li>
+        <li>
+          Trainees are liable to full responsibility for any property of the
+          company used during the period of the training program.{" "}
+        </li>
+        <li>
+          Keep all gadgets and electrical appliances clear from water, drinks,
+          or any form of liquid/fluid that may constitute danger or damage to
+          the gadgets.{" "}
+        </li>
+        <li>
+          Damages to any property of the company attract corresponding charges.
+          The company management retains the prerogative to deciding any
+          reconsideration about this.{" "}
+        </li>
+      </ul>
     </main>
   );
 };
