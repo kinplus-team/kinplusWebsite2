814d54b4c15d0d2381350a6e306d24671aaef3c3
AYD-7 <ayodejiaronimo@gmail.com>
2025-06-30 15:26:10 +0100

    Changed: FAQ UI writing

--- src/repository/FAQ.js
+++ src/repository/FAQ.js
@@ -21,24 +21,56 @@ const FAQsOnTrainingPage = [
   },
   {
     question:
-      "Do I need to have experience on my preferred training track before I can observe my IT/SIWES at Kinplus Technologies?",
+      "Do I need to have a knowledge or an experience on my preferred track before I can observe my IT/SIWES at Kinplus Technologies?",
     ans: {
       summary:
-        "Yes, you need to have at least basic knowledge on your prefered training track because you'll be working with other teammates. These include: Designers, Analysts, and Engineers(Developers).",
+        "Yes, you should have basic knowledge on your preferred training track because you will be integrated into our workforce while you learn on the job.",
     },
   },
   {
     question:
-      "What training tracks are available in the Academy training category?",
+      "What are the categories of training packages available at Kinplus Technologies?",
+    ans: {
+      summary: 
+        "We have 7 different training programs (courses) as listed on our training page. While the Basic Computer Operation course has a 1 month and another 2 months packages, each of the other courses have 3 months and 5 months packages, with varying prices and training content.",
+    }
+  },
+  {
+    question: 
+      "Is there certification for the Premium Training programs at Kinplus Technologies?",
     ans: {
       summary:
-        "We offer IT/SIWES training on only a limited number of courses. These include:",
-      options: ["Website Development", "Cyber-security", "Data Analytics", "Product(UI/UX) Design", "Graphics Design", "Product Management"],
+        "Yes. You will be certified if you enrol for any of our paid Premium Training programs. Our certificates are equally recognised and meet the required standards.",
+    },
+  },
+  {
+    question:
+      "Outside of Ado–Ekiti, where else can I get in contact with Kinplus Technologies?",
+    ans: {
+      summary:
+        "Kinplus Technologies has other centers outside Ado-Ekiti. A list of all our centers include:",
+      options: ["Ado – Ekiti", "Ikole – Ekiti", "Ido – Ekiti"],
     },
   },
 ];
 
 const FAQsOnHomePage = [
+  {
+    question:
+      "What are the categories of training packages available at Kinplus Technologies?",
+    ans: {
+      summary: 
+        "We have 7 different training programs (courses) as listed on our training page. While the Basic Computer Operation course has a 1 month and another 2 months packages, each of the other courses have 3 months and 5 months packages, with varying prices and training content.",
+    }
+  },
+  {
+    question: 
+      "Is there certification for the Premium Training programs at Kinplus Technologies?",
+    ans: {
+      summary:
+        "Yes. You will be certified if you enrol for any of our paid Premium Training programs. Our certificates are equally recognised and meet the required standards.",
+    },
+  },
   {
     question:
       "Outside of Ado–Ekiti, where else can I get in contact with Kinplus Technologies?",
@@ -59,29 +91,31 @@ const FAQsOnHomePage = [
     question: "Can I observe my IT/SIWES at Kinplus?",
     ans: {
       summary:
-        "Yes. At Kinplus Technologies, we accept students of various institutions to observe their statutory Industrial Training and Student Industrial Work Experience Scheme programs with our company. Our trainees undergo intensive training schedules, to the end so that they can put their learning into practical use. However, limited slots are available for each institution and phases of admission for IT/SIWES training. The company also makes extensive considerations towards the approval of each application.",
+        "Yes. At Kinplus Technologies, we accept students of various institutions to observe their statutory Industrial Training (IT) and Student Industrial Work Experience Scheme (SIWES) programs with our company. Although, there are usually limited slots each window.",
     },
   },
+  // {
+  //   question:
+  //     "Is IT/SIWES at Kinplus Technologies open to only students of tertiary institutions in Ekiti State?",
+  //   ans: {
+  //     summary:
+  //       "No, we accept students from various tertiary institutions. However, only a limited number of students can be accepted from each institution because we try to maintain a balance across board.",
+  //   },
+  // },
   {
     question:
-      "Is IT/SIWES at Kinplus Technologies open to only students of tertiary institutions in Ekiti State?",
+      "What training tracks are available in the Premium training category?",
     ans: {
       summary:
-        "No, we accept students from various tertiary institutions. However, only a limited number of students can be accepted from each institution because we try to maintain a balance across board.",
-    },
-  },
-  {
-    question:
-      "What training tracks are available in the Academy training category?",
-    ans: {
-      summary:
-        "Kinplus Technologies offers training for Academy students on the following tracks:",
+        "Kinplus Technologies offers training for Premium students on the following tracks:",
       options: [
         "Software Development",
         "Product Design (UI/UX)",
         "Data Analysis",
         "Cyber-security",
         "Graphics Design",
+        "Digital Marketing",
+        "Basic Computer Operation",
       ],
     },
   },
